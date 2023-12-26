0514f4418a2dd9d25baa44410e01a090c695e37a
fauzanikhsani <fauzanikhsani@student.telkomuniversity.ac.id>
2023-12-27 00:24:54 +0700

    commit make loginPage

---
+++ fe/src/pages/loginPage/loginPage.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import Navbar from "../../components/navbar/Navbar"
+
+function loginPage() {
+    return (
+        <Navbar/>
+    )
+}
+
+export default loginPage;
