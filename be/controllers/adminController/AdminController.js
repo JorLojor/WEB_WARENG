8a90798a8f1582891d90fd9945311373c24dff03
JorLojor <nugas123man@gmail.com>
2024-01-01 02:57:23 +0700

    seep

--- be/controllers/adminController/AdminController.js
+++ be/controllers/adminController/AdminController.js
@@ -1,6 +1,5 @@
 const db = require('../../models');
 const WargaModel = db.warga;
-const SuratAcaraModel = db.suratAcara;
 const rtModel = db.rt;
 const rwModel = db.rw;
 const pdModel = db.PerangkatDesaModel;
@@ -8,3 +7,86 @@ const ppModel = db.pimpinanDesa;
 const userModel = db.user;
 
 // CRUD OPERATIONS
+
+// CREATE
+exports.postAdmin = async (req, res) => {
+    try{
+        const {name, nik, nohp, username, password} = req.body;
+
+        const newAdmin = new AdminModel({
+            name,
+            nik,
+            nohp,
+            username,
+            password
+        });
+
+        const admin = await newAdmin.save();
+
+        res.status(201).send({
+            message: "success create admin",
+            admin
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+}
+
+// READ
+exports.getAdminById = async (req, res) => {
+    try{
+        const {id} = req.params;
+
+        const admin = await AdminModel.findById(id);
+
+        res.status(200).send({
+            message: "success get admin by id",
+            admin
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+}
+
+// UPDATE
+exports.updateAdmin = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {name, nik, nohp, username, password} = req.body;
+
+        const admin = await AdminModel.findByIdAndUpdate(id, {
+            name,
+            nik,
+            nohp,
+            username,
+            password
+        }, {new: true});
+
+        res.status(200).send({
+            message: "success update admin",
+            admin
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+}
+
+// DELETE
+exports.deleteAdmin = async (req, res) => {
+    try{
+        const {id} = req.params;
+
+        const admin = await AdminModel.findByIdAndRemove(id);
+
+        res.status(200).send({
+            message: "success delete admin",
+            admin
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+}
--- be/models/adminModels/adminModels.js
+++ be/models/adminModels/adminModels.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const admin = new mongoose.Schema({
     name: { type: String, required: true },
     nik: { type:Number, required: true, default: '',unique: true},
+    nohp: { type:Number, required: true, default: ''},
+    userName : { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     password: { type:String, required: true, default: ''},
-    user : { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
 },{timestamp: true});
 
 module.exports = mongoose.model('admin', admin);
