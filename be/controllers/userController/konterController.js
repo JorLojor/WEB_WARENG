bb9663f48ab46def2fad1b23e817d8b9267fbb12
JorLojor <nugas123man@gmail.com>
2024-04-03 19:16:52 +0700

    lanjut nanti

--- be/controllers/userController/konterController.js
+++ be/controllers/userController/konterController.js
@@ -2,15 +2,65 @@ const db = require('../models');
 
 
 
-exports.getDataKonter = async (req, res) => {};
+exports.getDataKonter = async (req, res) => {
+    try{
+        // nyusull
 
-exports.getDataKonterById = async (req, res) => {};
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({
+            message: error.message || "Some error occurred while get all konter."
+        });
+    }
+};
 
-exports.createKonter = async (req, res) => {};
+exports.getDataKonterById = async (req, res) => {
+    try{
+        // nyusull
 
-exports.updateKonter = async (req, res) => {};
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({
+            message: error.message || "Some error occurred while get konter by id."
+        });
+    }
+};
 
-exports.deleteKonter = async (req, res) => {};
+exports.createKonter = async (req, res) => {
+    try{
+        // nyusull
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({
+            message: error.message || "Some error occurred while create konter."
+        });
+    }
+};
+
+exports.updateKonter = async (req, res) => {
+    try{
+        // nyusull
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({
+            message: error.message || "Some error occurred while update konter."
+        });
+    }
+};
+
+exports.deleteKonter = async (req, res) => {
+    try{
+        // nyusull
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({
+            message: error.message || "Some error occurred while delete konter."
+        });
+    }
+};
 
 
 
