8988396f2fad2a919f20af2f922ea752ab6d10f4
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2024-05-03 16:42:12 +0700

    feat/informasiDesa add tentang session, visimisi session,

--- fe/src/sid/pages/informasiDesa/index.jsx
+++ fe/src/sid/pages/informasiDesa/index.jsx
@@ -5,18 +5,28 @@ import { Fragment } from "react";
 import Footer from "../../../shared/layout/footer";
 import Navbar from "../../../shared/layout/navBar";
 import Tentang from "./session/tentang";
-import "./index.css";
+import VisiMisi from "./session/visiMisi";
+import KegiatanDesa from "./session/kegiatanDesa";
 
 const InformasiDesa = () => {
 
     return (
         <Fragment>
-            <div className="conatiner-fluid informasi-desa-container">
+            <div className="conatiner-fluid informasi-desa-container" style={{overflow:'hidden'}}>
                 <Navbar type={0}></Navbar>
-                    <Tentang />
+                <div className="row">
+                    <div className="col-0 col-md-1"></div>
+                    <div className="col-12 col-md-10">
+                        <Tentang />
+                        <VisiMisi />
+                        <KegiatanDesa />
+                    </div>
+                    <div className="col-0 col-md-1">
+                    </div>
+                </div>
                 <Footer type={2}></Footer>
             </div>
-        </Fragment>
+        </Fragment >
     )
 }
 
